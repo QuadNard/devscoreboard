0bae22cee71fa1e5857ff044197690058d52b276
QuadNard <lizardkingnardo@gmail.com>
2023-10-17 19:06:18 -0400

    ⏩ created constant file

---
+++ src/utils/contants.js
@@ -0,0 +1,16 @@
+const MAX_LEVEL = 55;
+
+const MAX_PRESTIGIOUS_LEVEL = 7;
+
+// Define an array of points required for each level (customize as needed)
+const pointsPerLevel = [
+  100, 200, 300, 400, 500, // Levels 1-5
+  600, 700, 800, 900, 1000, // Levels 6-10
+  // Continue adding points for levels 11-55 as needed
+];
+
+export {
+    MAX_LEVEL,
+    MAX_PRESTIGIOUS_LEVEL,
+    pointsPerLevel,
+}
