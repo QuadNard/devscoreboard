1e69ab4d7c7339d1fd3a251bd88d262d7ec36511
QuadNard <lizardkingnardo@gmail.com>
2023-10-19 17:01:33 -0400

    created start menu UI

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Layout = ({ children }) => {
+    
+    return (
+      <div>
+        {children}
+      </div>
+    )
+}
+
+export default Layout;
---
+++ src/components/ui/NotificationMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationMenu = () => {
+    return (
+        <div className=''>
+        
+        </div>
+    )
+}
+
+export default NotificationMenu
