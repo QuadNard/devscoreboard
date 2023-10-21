31b78fa426d01f7fcc7d603a88f280c1b5bc624f
QuadNard <lizardkingnardo@gmail.com>
2023-10-21 18:55:53 -0400

    ⚒ set webhooks from db

--- src/pages/QuizSession.jsx
+++ src/pages/QuizSession.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+
 import Modal from '../components/ui/Modal';
 import Header from '../components/ui/Header';
 import usePlayerState from '../services/state/Store';
+import useBlitzModeData from '../hooks/useBlitzQuiz';
+import { db } from '../services/db/Firebase'
 
 function QuizSession(props) {
   const { level, prestigiousLevel } = usePlayerState();
 
-  console.log(props.selectedQuizMode);
+   const blitzModeData = useBlitzModeData(db);
+
+   console.log(blitzModeData);
   return (
     <div className="flex min-h-screen flex-col">
       <Header playerLevel={level} prestige={prestigiousLevel} />
